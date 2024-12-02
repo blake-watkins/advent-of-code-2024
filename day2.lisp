95c84e0a143bf70add549acc409a3bba038d2b8d
Blake Watkins <blakewatkins@gmail.com>
2024-12-03 07:42:54 +1300

    Part 2

--- day2.lisp
+++ day2.lisp
@@ -1,20 +1,24 @@
 (in-package :aoc-2024)
 
-(defun parse-numbers ()
-  (parse-lines (parse-number-list #\Space)))
-
-(defun safe (levels)
+(defun is-safe (levels)
   (or (<= (length levels) 1)
       (iter
         (with increasing = (> (second levels) (first levels)))
         (while (>= (length levels) 2))
         (for diff = (- (second levels) (first levels)))
-        (always (if increasing
-                    (<= 1 diff 3)
-                    (<= -3 diff -1)))
+        (always (<= 1 (if increasing diff (- diff)) 3))
         (setf levels (rest levels)))))
+
+(defun is-dampened-safe (levels)
+  (or (is-safe levels)
+      (iter
+        (for i below (length levels))
+        (for dampened = (concatenate 'list
+                                     (subseq levels 0 i)
+                                     (subseq levels (1+ i))))
+        (thereis (is-safe dampened)))))
 
 (defun day2 (input &key (part 1))
   (iter
-    (for levels in (run-parser (parse-numbers) input))
-    (counting (safe levels))))
+    (for levels in (run-parser (parse-lines (parse-number-list #\Space)) input))
+    (counting (if (= part 1) (is-safe levels) (is-dampened-safe levels)))))
