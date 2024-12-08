2c61e7c7ee935b349c6f6afe9d58dee280411f7d
Blake Watkins <blakewatkins@gmail.com>
2024-12-08 17:41:19 +1300

    Abstract operations.

--- day7.lisp
+++ day7.lisp
@@ -11,15 +11,17 @@
 (defun concat (a b)
   (parse-integer (format nil "~a~a" a b)))
 
-(defun test-target (target cur numbers)
+(defun test-target (target cur numbers ops)
   (if numbers
-      (or (test-target target (+ cur (first numbers)) (rest numbers))
-          (test-target target (* cur (first numbers)) (rest numbers))
-          (test-target target (concat cur (first numbers))  (rest numbers)))
+      (iter
+        (for op in ops)
+        (thereis
+         (test-target target (funcall op cur (first numbers)) (rest numbers) ops)))
       (= target cur)))
 
-(defun day7 (input)
+(defun day7 (input &key (part 1))  
   (iter
+    (with ops = (if (= part 1) (list #'* #'+) (list #'* #'+ #'concat)))
     (for (target numbers) in (run-parser (parse-equations) input))
-    (when (test-target target 0 numbers)
+    (when (test-target target 0 numbers ops)
       (sum target))))
